3a6b43b27f7aa81fbc2f429bdfdf49eaa00bb595
jeanarthur <jean.arthursa@gmail.com>
2025-03-09 10:51:30 -0300

    feat: adiciona imagens de capa para cada conteudo

--- lib/data/services/local/local_data_service.dart
+++ lib/data/services/local/local_data_service.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 class LocalDataService {
   List<QuizBook> getQuizBookList() {
     return [
-      QuizBook(id: 0, title: 'Matemática', color: Colors.blue),
+      QuizBook(id: 0, title: 'Matemática', imagePath: 'assets/images/mathematics_quiz_cover.png', color: Colors.blue),
       QuizBook(id: 1, title: 'Língua Portuguesa', imagePath: 'assets/images/portuguese_quiz_cover.png', color: Colors.red),
-      QuizBook(id: 2, title: 'Inglês', color: Colors.orange),
-      QuizBook(id: 3, title: 'Geografia', color: Colors.brown),
+      QuizBook(id: 2, title: 'Inglês', imagePath: 'assets/images/english_quiz_cover.png', color: Colors.orange),
+      QuizBook(id: 3, title: 'Geografia', imagePath: 'assets/images/geography_quiz_cover.png', color: Colors.brown),
     ];
   }
 }
